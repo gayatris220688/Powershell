ed3e989d30d0d9416edcb1aa6fcfb4cd069fcada
Gayatri Sanap <v-gasana@microsoft.com>
2019-12-03 15:09:00 +0530

    fjfjkk

---
+++ Array -shane.ps1
@@ -0,0 +1,13 @@
+﻿$array = @()
+
+$array
+
+$processes = Get-Process
+
+foreach ($proc in $processes)
+{
+    If ($proc.ws/1mb -gt 100)
+    {
+        $array  += New-Object psobject -Property @{'ProcessName' = $proc.name ; 'Workingset' = $proc.ws }
+    }
+}
